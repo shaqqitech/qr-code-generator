27b665cc0d8ed427d3dbc073719239cca2c1830f
Mehar Ali <mehar0alii@gmail.com>
2023-08-19 11:00:22 +0500

    add download functionality

--- src/components/QRCodeGenerator.jsx
+++ src/components/QRCodeGenerator.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
-import QRCode from 'qrcode.react'; // npm i qrcode.react 
+import QRCode from 'qrcode.react';
 
 function QRCodeGenerator() {
   const [text, setText] = useState('');
+
+  const handleDownload = () => {
+    const canvas = document.querySelector('canvas');
+    const url = canvas.toDataURL('image/png');
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = 'qrcode.png';
+    anchor.click();
+  };
 
   return (
     <div className="flex flex-col items-center mt-10 border-2 border-black rounded-lg p-10 shadow-2xl">
@@ -14,7 +23,17 @@ function QRCodeGenerator() {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      {text && <QRCode value={text} size={200} />}
+      {text && (
+        <div className="flex flex-col items-center">
+          <QRCode value={text} size={200} />
+          <button
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+            onClick={handleDownload}
+          >
+            Download QR Code
+          </button>
+        </div>
+      )}
     </div>
   );
 }
